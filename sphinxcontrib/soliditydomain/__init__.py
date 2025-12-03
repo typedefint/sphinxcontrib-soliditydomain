1f626fc958c9a6671d073c6746a05acf56306452
Tobias Oberstein <tobias.oberstein@gmail.com>
2025-12-03 09:46:44 +0100

    chore: remove unused patch and apply ruff formatting
    - Remove domain-fix-fullnameid.patch (not needed for Sphinx 8+ compatibility)
    - Apply ruff format to non-generated Python files for consistent code style
    - Keep ANTLR-generated files (SolidityLexer.py, SolidityParser.py) unchanged
    
    Note: This work was completed with AI assistance (Claude Code).

--- sphinxcontrib/soliditydomain/__init__.py
+++ sphinxcontrib/soliditydomain/__init__.py
@@ -4,18 +4,18 @@ from .domain import SolidityDomain
 from .documenters import all_solidity_documenters
 from .sourceregistry import build_source_registry, teardown_source_registry
 
-with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as version_file:
+with open(os.path.join(os.path.dirname(__file__), "VERSION")) as version_file:
     __version__ = version_file.read().strip()
 
+
 def setup(app):
-    app.add_config_value('autodoc_lookup_path',
-                         os.path.join('..', 'contracts'), 'env')
+    app.add_config_value("autodoc_lookup_path", os.path.join("..", "contracts"), "env")
 
     app.add_domain(SolidityDomain)
 
-    app.connect('builder-inited', build_source_registry)
-    app.connect('env-before-read-docs', read_all_docs)
-    app.connect('build-finished', teardown_source_registry)
+    app.connect("builder-inited", build_source_registry)
+    app.connect("env-before-read-docs", read_all_docs)
+    app.connect("build-finished", teardown_source_registry)
 
     for documenter in all_solidity_documenters.values():
         app.add_autodocumenter(documenter)
--- sphinxcontrib/soliditydomain/documenters.py
+++ sphinxcontrib/soliditydomain/documenters.py
@@ -1,21 +1,26 @@
 import re
 from sphinx.ext.autodoc import (
-    ALL, Documenter,
-    bool_option, members_option, exclude_members_option)
+    ALL,
+    Documenter,
+    bool_option,
+    members_option,
+    exclude_members_option,
+)
 from .domain import SolidityDomain
 from .sourceregistry import SolidityObject
 
 from sphinx.util.logging import getLogger
+
 logger = getLogger(__name__)
 
 
 class SolidityObjectDocumenter(Documenter):
-    domain = 'sol'
+    domain = "sol"
     option_spec = {
-        'members': members_option,
-        'undoc-members': bool_option,
-        'noindex': bool_option,
-        'exclude-members': exclude_members_option,
+        "members": members_option,
+        "undoc-members": bool_option,
+        "noindex": bool_option,
+        "exclude-members": exclude_members_option,
     }
 
     @classmethod
@@ -27,38 +32,41 @@ class SolidityObjectDocumenter(Documenter):
         return False
 
     def get_sourcename(self):
-        return '{}:docstring of {} {}'.format(
+        return "{}:docstring of {} {}".format(
             self.object.file,
             self.object.objtype,
-            '.'.join(filter(lambda x: x,
-                            (self.object.contract_name,
-                             self.object.name))),
+            ".".join(
+                filter(lambda x: x, (self.object.contract_name, self.object.name))
+            ),
         )
 
     def add_directive_header(self):
-        domain = getattr(self, 'domain', 'sol')
-        directive = getattr(self, 'directivetype', self.objtype)
+        domain = getattr(self, "domain", "sol")
+        directive = getattr(self, "directivetype", self.objtype)
         sourcename = self.get_sourcename()
 
-        self.add_line('.. {domain}:{directive}:: {signature}'.format(
-            domain=domain, directive=directive, signature=self.object.signature
-        ), sourcename)
+        self.add_line(
+            ".. {domain}:{directive}:: {signature}".format(
+                domain=domain, directive=directive, signature=self.object.signature
+            ),
+            sourcename,
+        )
 
         if self.options.noindex:
-            self.add_line(u'   :noindex:', sourcename)
+            self.add_line("   :noindex:", sourcename)
 
     def add_content(self, more_content):
         """Add content from source docs and user."""
         sourcename = self.get_sourcename()
 
         if self.object.docs:
-            self.add_line('', sourcename)
+            self.add_line("", sourcename)
             for line in self.object.docs.splitlines():
                 self.add_line(line, sourcename)
 
         # add additional content (e.g. from document), if present
         if more_content:
-            self.add_line('', sourcename)
+            self.add_line("", sourcename)
             for line, src in zip(more_content.data, more_content.items):
                 self.add_line(line, src[0], src[1])
 
@@ -78,7 +86,7 @@ class SolidityObjectDocumenter(Documenter):
 
         expressions = [
             SolidityObject.file == self.object.file,
-            SolidityObject.contract_name == self.object.name
+            SolidityObject.contract_name == self.object.name,
         ]
 
         if not want_all:
@@ -87,18 +95,22 @@ class SolidityObjectDocumenter(Documenter):
             should_include_constructor = False
 
             for member in self.options.members:
-                if member == '<fallback>':
+                if member == "<fallback>":
                     should_include_fallback = True
-                elif member == 'constructor':
+                elif member == "constructor":
                     should_include_constructor = True
                 elif member:
                     members_inset.add(member)
 
             expr = SolidityObject.name.in_(members_inset)
             if should_include_fallback:
-                expr |= (SolidityObject.objtype == 'function') & (SolidityObject.name.is_null(True))
+                expr |= (SolidityObject.objtype == "function") & (
+                    SolidityObject.name.is_null(True)
+                )
             if should_include_constructor:
-                expr |= (SolidityObject.objtype == 'constructor') & (SolidityObject.name.is_null(True))
+                expr |= (SolidityObject.objtype == "constructor") & (
+                    SolidityObject.name.is_null(True)
+                )
 
             expressions.append(expr)
 
@@ -106,42 +118,49 @@ class SolidityObjectDocumenter(Documenter):
             should_exclude_fallback = False
             should_exclude_constructor = False
 
-            if '<fallback>' in self.options.exclude_members:
-                self.options.exclude_members.remove('<fallback>')
+            if "<fallback>" in self.options.exclude_members:
+                self.options.exclude_members.remove("<fallback>")
                 should_exclude_fallback = True
-            if 'constructor' in self.options.exclude_members:
-                self.options.exclude_members.remove('constructor')
+            if "constructor" in self.options.exclude_members:
+                self.options.exclude_members.remove("constructor")
                 should_exclude_constructor = True
 
             expr = SolidityObject.name.not_in(self.options.exclude_members)
 
             subexpr = SolidityObject.name.is_null(True)
             if should_exclude_fallback:
-                subexpr &= (SolidityObject.objtype != 'function')
+                subexpr &= SolidityObject.objtype != "function"
             if should_exclude_constructor:
-                subexpr &= (SolidityObject.objtype != 'constructor')
+                subexpr &= SolidityObject.objtype != "constructor"
             expr |= subexpr
 
             expressions.append(expr)
 
-        should_exclude_private = self.options.exclude_members and '<private>' in self.options.exclude_members
+        should_exclude_private = (
+            self.options.exclude_members and "<private>" in self.options.exclude_members
+        )
         for member in SolidityObject.select().where(*expressions):
-            if member.objtype == 'function':
-                is_public = 'public' in member.signature or 'external' in member.signature
+            if member.objtype == "function":
+                is_public = (
+                    "public" in member.signature or "external" in member.signature
+                )
                 if should_exclude_private and not is_public:
                     continue
 
-            self.add_line('', sourcename)
-            full_mname = '{file}:{contract}{name}{paramtypes}'.format(
+            self.add_line("", sourcename)
+            full_mname = "{file}:{contract}{name}{paramtypes}".format(
                 file=member.file,
-                contract='' if member.contract_name is None
-                else member.contract_name + '.',
-                name=member.name or '',
-                paramtypes='' if member.paramtypes is None
-                else '(' + member.paramtypes + ')',
+                contract=""
+                if member.contract_name is None
+                else member.contract_name + ".",
+                name=member.name or "",
+                paramtypes=""
+                if member.paramtypes is None
+                else "(" + member.paramtypes + ")",
             )
             documenter = all_solidity_documenters[member.objtype](
-                self.directive, full_mname, self.indent)
+                self.directive, full_mname, self.indent
+            )
             documenter.generate(all_members=True)
 
     def generate(self, more_content=None, all_members=False):
@@ -152,23 +171,23 @@ class SolidityObjectDocumenter(Documenter):
         If *more_content* is given, include that content.
         If *all_members* is True, document all members.
         """
-        directive = getattr(self, 'directivetype', self.objtype)
+        directive = getattr(self, "directivetype", self.objtype)
 
         # parse components out of name
-        (file, _, namepath) = self.name.rpartition(':')
-        (contract_name, _, fullname) = namepath.partition('.')
-        (name, _, paramtypes) = fullname.partition('(')
+        (file, _, namepath) = self.name.rpartition(":")
+        (contract_name, _, fullname) = namepath.partition(".")
+        (name, _, paramtypes) = fullname.partition("(")
 
         # normalize components
         name = name.strip() or None
 
-        if directive in ('contract', 'interface', 'library') and name is None:
+        if directive in ("contract", "interface", "library") and name is None:
             name = contract_name
             contract_name = None
 
-        paramtypes = ','.join(ptype.strip() for ptype in paramtypes.split(','))
-        paramtypes = re.sub(r'\s+', ' ', paramtypes)
-        if paramtypes.endswith(')'):
+        paramtypes = ",".join(ptype.strip() for ptype in paramtypes.split(","))
+        paramtypes = re.sub(r"\s+", " ", paramtypes)
+        if paramtypes.endswith(")"):
             paramtypes = paramtypes[:-1]
 
         # build query
@@ -188,17 +207,28 @@ class SolidityObjectDocumenter(Documenter):
         query = SolidityObject.select().where(*expressions)
         sol_objects = tuple(query)
         if len(sol_objects) == 0:
-            logger.warning('{} {} could not be found via query:\n{}'.format(
-                directive, self.name, ',\n'.join(
-                    '  ' + str(expr.lhs.column_name) +
-                    str(expr.op) + ('' if expr.rhs is None else expr.rhs)
-                    for expr in expressions
-                )))
+            logger.warning(
+                "{} {} could not be found via query:\n{}".format(
+                    directive,
+                    self.name,
+                    ",\n".join(
+                        "  "
+                        + str(expr.lhs.column_name)
+                        + str(expr.op)
+                        + ("" if expr.rhs is None else expr.rhs)
+                        for expr in expressions
+                    ),
+                )
+            )
             return
         elif len(sol_objects) > 1:
-            logger.warning('multiple candidates for {} {} found:\n{}'.format(
-                directive, self.name,
-                '\n'.join('  ' + obj.signature for obj in sol_objects)))
+            logger.warning(
+                "multiple candidates for {} {} found:\n{}".format(
+                    directive,
+                    self.name,
+                    "\n".join("  " + obj.signature for obj in sol_objects),
+                )
+            )
 
         self.object = sol_objects[0]
 
@@ -208,7 +238,7 @@ class SolidityObjectDocumenter(Documenter):
         # make sure that the result starts with an empty line.  This is
         # necessary for some situations where another directive preprocesses
         # reST and no starting newline is present
-        self.add_line('', sourcename)
+        self.add_line("", sourcename)
 
         # generate the directive header and options, if applicable
         self.add_directive_header()
@@ -221,8 +251,8 @@ class SolidityObjectDocumenter(Documenter):
         self.add_content(more_content)
 
         # document members, if possible
-        if directive in ('contract', 'interface', 'library'):
-            self.add_line('', sourcename)
+        if directive in ("contract", "interface", "library"):
+            self.add_line("", sourcename)
             self.document_members(all_members)
 
 
@@ -231,20 +261,32 @@ def method_stub(self):
 
 
 for method_name in (
-    'parse_name', 'import_object', 'get_real_modname', 'check_module',
-    'format_args', 'format_name', 'format_signature', 'get_doc', 'process_doc',
-    'get_object_members', 'filter_members',
+    "parse_name",
+    "import_object",
+    "get_real_modname",
+    "check_module",
+    "format_args",
+    "format_name",
+    "format_signature",
+    "get_doc",
+    "process_doc",
+    "get_object_members",
+    "filter_members",
 ):
     setattr(SolidityObjectDocumenter, method_name, method_stub)
 
 
 all_solidity_documenters = dict(
-    (objtype, type(
-        objtype.capitalize() + 'Documenter',
-        (SolidityObjectDocumenter,),
-        {
-            'objtype': 'sol' + objtype,
-            'directivetype': objtype,
-        }
-    )) for objtype in SolidityDomain.directives.keys()
+    (
+        objtype,
+        type(
+            objtype.capitalize() + "Documenter",
+            (SolidityObjectDocumenter,),
+            {
+                "objtype": "sol" + objtype,
+                "directivetype": objtype,
+            },
+        ),
+    )
+    for objtype in SolidityDomain.directives.keys()
 )
